f66e701fdf94c957a15d3b22b997102dd8cc9ecf
Timothy Kephart <tim_kephart2006@yahoo.com>
2015-07-12 00:51:42 -0700

    Main Menu build 1

---
+++ ProjectYore/ProjectYore/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace MainMenu
+{
+    public class MainMenu : MonoBehaviour
+    {
+
+        public Canvas quitMenu;
+        public Button startText;
+        public Button exitText;
+
+        void Start()
+        {
+            quitMenu = quitMenu.GetComponent<Canvas>();
+            quitMenu.enabled = false;
+            startText = startText.GetComponent<Button>();
+            exitText = exitText.GetComponent<Button>();
+        }
+
+        public void ExitPressed()
+        {
+            Debug.Log("Exit game?");
+            quitMenu.enabled = true;
+            startText.enabled = false;
+            exitText.enabled = false;
+        }
+
+        public void ReturnToMainMenu()
+        {
+            Debug.Log("Return to main menu");
+            quitMenu.enabled = false;
+            startText.enabled = true;
+            exitText.enabled = true;
+        }
+
+        public void StartLevel()
+        {
+            Debug.Log("Start level");
+            Application.LoadLevel(1);
+        }
+
+        public void ExitGame()
+        {
+            Debug.Log("Exit game");
+            Application.Quit();
+        }
+
+    }
+}
